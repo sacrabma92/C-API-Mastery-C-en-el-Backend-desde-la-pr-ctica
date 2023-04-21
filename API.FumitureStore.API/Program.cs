56c4f8e43ec6e47cf1791bd7e6d6a8ff1100b7cc
Carlos Ramirez <karlossander92@gmail.com>
2023-04-21 15:21:54 -0500

    Capa Acceso a Datos

--- API.FumitureStore.API/Program.cs
+++ API.FumitureStore.API/Program.cs
@@ -1,3 +1,6 @@
+using API.FumitureStore.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<APIFurnitureStoreContext>(options =>
+                options.UseSqlite(builder.Configuration.GetConnectionString("APIFirnitureStoreContext")));
 
 var app = builder.Build();
 
---
+++ API.FumitureStore.Data/APIFurnitureStoreContext.cs
@@ -0,0 +1,23 @@
+﻿using API.FumitureStore.Shared;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.FumitureStore.Data
+{
+    public class APIFurnitureStoreContext : DbContext
+    {
+        public APIFurnitureStoreContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Client> Clients { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Order> Orders{ get; set; }
+        public DbSet<ProductCategory> ProductCategories{ get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite();
+        }
+    }
+}
