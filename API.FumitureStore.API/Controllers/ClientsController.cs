60c259e21b05c02c5cc0f128dc144bbf115c75e4
Carlos Ramirez <karlossander92@gmail.com>
2023-04-22 08:37:09 -0500

    Falta Corregir Errores DE Guardado

---
+++ API.FumitureStore.API/Controllers/ClientsController.cs
@@ -0,0 +1,69 @@
+﻿using API.FumitureStore.Data;
+using API.FumitureStore.Shared;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.FumitureStore.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClientsController : ControllerBase
+    {
+        private readonly APIFurnitureStoreContext _context;
+
+        public ClientsController(APIFurnitureStoreContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<Client>> GetClient()
+        {
+            return await _context.Clients.ToListAsync();
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<IActionResult> GetClientDetails(int id)
+        {
+            var client = await _context.Clients.FirstOrDefaultAsync( c => c.Id == id);
+
+            if(client == null) 
+            {
+                return NotFound();
+            }
+
+            return Ok(client);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> PostCliente(Client client)
+        {
+            await _context.Clients.AddAsync(client);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction("Post", client.Id, client);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> PutClient(Client client)
+        {
+            _context.Clients.Update(client);
+
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> DeleteClient(Client client)
+        {
+            if(client == null) return NotFound();
+
+            _context.Clients.Remove(client);
+
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+    }
+}
---
+++ API.FumitureStore.API/Controllers/ProductCategoryController.cs
@@ -0,0 +1,70 @@
+﻿using API.FumitureStore.Data;
+using API.FumitureStore.Shared;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.FumitureStore.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductCategoryController : ControllerBase
+    {
+        private readonly APIFurnitureStoreContext _context;
+
+        public ProductCategoryController(APIFurnitureStoreContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<ProductCategory>> GetProductCategories()
+        {
+            return await _context.ProductCategories.ToListAsync();
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<IActionResult> GetCategoryDetails(int id)
+        {
+            var category = await _context.ProductCategories.FirstOrDefaultAsync( p => p.Id == id);
+
+            if(category == null) 
+            {
+                return NotFound();
+            }
+
+            return Ok(category);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> PostProductCategory(ProductCategory productCategory)
+        {
+            await _context.ProductCategories.AddAsync(productCategory);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("Post", productCategory.Id, productCategory);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> PutCategory(ProductCategory productCategory)
+        {
+            _context.ProductCategories.Update(productCategory);
+
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> DeleteClient(ProductCategory productCategory)
+        {
+            if(productCategory== null) return NotFound();
+
+            _context.ProductCategories.Remove(productCategory);
+
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+    }
+}
